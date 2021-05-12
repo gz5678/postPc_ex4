a51be987c77e66ad4e5c92206ce382f096b56153
gz5678 <gil.zivor@mail.huji.ac.il>
2021-05-12 15:00:20 +0300

    Edit README and answered question. Final Commit.

--- app/src/main/java/exercise/find/roots/MainActivity.java
+++ app/src/main/java/exercise/find/roots/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         editTextUserInput.setEnabled(true);
         buttonCalculateRoots.setEnabled(true);
         int timeUntilTimeout = (int) incomingIntent.getIntExtra("time_until_give_up_seconds", -1);
-        String msg = "Calculation aborted after " + String.valueOf(timeUntilTimeout) + " seconds";
+        String msg = "Calculation aborted after " + timeUntilTimeout + " seconds";
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
       }
     };
@@ -145,49 +145,3 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 }
-
-
-/*
-
-TODO:
-the spec is:
-
-upon launch, Activity starts out "clean":
-* progress-bar is hidden
-* "input" edit-text has no input and it is enabled
-* "calculate roots" button is disabled
-
-the button behavior is:
-* when there is no valid-number as an input in the edit-text, button is disabled
-* when we triggered a calculation and still didn't get any result, button is disabled
-* otherwise (valid number && not calculating anything in the BG), button is enabled
-
-the edit-text behavior is:
-* when there is a calculation in the BG, edit-text is disabled (user can't input anything)
-* otherwise (not calculating anything in the BG), edit-text is enabled (user can tap to open the keyboard and add input)
-
-the progress behavior is:
-* when there is a calculation in the BG, progress is showing
-* otherwise (not calculating anything in the BG), progress is hidden
-
-when "calculate roots" button is clicked:
-* change states for the progress, edit-text and button as needed, so user can't interact with the screen
-
-when calculation is complete successfully:
-* change states for the progress, edit-text and button as needed, so the screen can accept new input
-* open a new "success" screen showing the following data:
-  - the original input number
-  - 2 roots combining this number (e.g. if the input was 99 then you can show "99=9*11" or "99=3*33"
-  - calculation time in seconds
-
-when calculation is aborted as it took too much time:
-* change states for the progress, edit-text and button as needed, so the screen can accept new input
-* show a toast "calculation aborted after X seconds"
-
-
-upon screen rotation (saveState && loadState) the new screen should show exactly the same state as the old screen. this means:
-* edit-text shows the same input
-* edit-text is disabled/enabled based on current "is waiting for calculation?" state
-* progress is showing/hidden based on current "is waiting for calculation?" state
-* button is enabled/disabled based on current "is waiting for calculation?" state && there is a valid number in the edit-text input
- */
