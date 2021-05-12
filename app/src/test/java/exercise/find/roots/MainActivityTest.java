c8282f608ae73aeea3488653996808bd1562b325
gz5678 <gil.zivor@mail.huji.ac.il>
2021-05-12 13:57:25 +0300

    Added 2 more unittests to MainActivityTest

--- app/src/test/java/exercise/find/roots/MainActivityTest.java
+++ app/src/test/java/exercise/find/roots/MainActivityTest.java
@@ -1,7 +1,9 @@
 package exercise.find.roots;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import junit.framework.TestCase;
 
@@ -45,13 +47,39 @@ public class MainActivityTest extends TestCase {
     EditText inputEditText = mainActivity.findViewById(R.id.editTextInputNumber);
     Button button = mainActivity.findViewById(R.id.buttonCalculateRoots);
 
+    // enter good number and check that button is enabled
     inputEditText.setText("1234567");
     assertTrue(button.isEnabled());
   }
 
+  public void when_mainActivityLaunches_then_progressBarIsHidden() {
+    // create a MainActivity and let it think it's currently displayed on the screen
+    MainActivity mainActivity = Robolectric.buildActivity(MainActivity.class).create().visible().get();
+
+    // find the progress bar and check that it's hidden
+    ProgressBar progressBar = mainActivity.findViewById(R.id.progressBar);
+    assertEquals(progressBar.getVisibility(), View.GONE);
+  }
+
+  public void when_insertGoodNumber_and_clickButton_then_progressBarIsDisplayed() {
+    // create a MainActivity and let it think it's currently displayed on the screen
+    MainActivity mainActivity = Robolectric.buildActivity(MainActivity.class).create().visible().get();
+
+    // find the edit-text, button and progress bar
+    EditText inputEditText = mainActivity.findViewById(R.id.editTextInputNumber);
+    Button button = mainActivity.findViewById(R.id.buttonCalculateRoots);
+    ProgressBar progressBar = mainActivity.findViewById(R.id.progressBar);
+
+    // enter good number and click button
+    inputEditText.setText("1234567");
+    button.performClick();
+
+    // assert that progress bar is visible
+    assertEquals(progressBar.getVisibility(), View.VISIBLE);
+  }
+
   // TODO: add 1 or 2 more unit tests to the activity. so your "writing tests" skill won't get rusty.
   //  possible flows to unit-test:
-  //  - when activity launches, "progress" starts hidden
   //  - when inserting a good number and clicking the button, "progress" should be displayed
   //  - when user is entering a bad input (for example "17.3") the button should be disabled
   //  - when user is entering a good input and than deleting it, the button should be enabled and then disabled again
